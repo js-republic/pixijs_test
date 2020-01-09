fed5826b1eeab08fffa804527ce6d8a6897e0df7
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2020-01-09 16:11:20 +0100

    add logo animation

--- logo.js
+++ logo.js
@@ -1,85 +1,123 @@
 import * as PIXI from "pixi.js";
 
-const app = new PIXI.Application();
+const app = new PIXI.Application({
+  backgroundColor: 0xffffff
+});
 document.body.appendChild(app.view);
 
-// Get the texture for rope.
-const starTexture = PIXI.Texture.from(
-  "https://pixijs.io/examples/examples/assets/star.png"
-);
+class Line extends PIXI.Graphics {
+  constructor(points, lineSize, lineColor) {
+    super();
 
-const starAmount = 1000;
-let cameraZ = 0;
-const fov = 20;
-const baseSpeed = 0.025;
-let speed = 0;
-let warpSpeed = 0;
-const starStretch = 5;
-const starBaseSize = 0.05;
+    var s = (this.lineWidth = lineSize || 5);
+    var c = (this.lineColor = lineColor || "0x000000");
 
-// Create the stars
-const stars = [];
-for (let i = 0; i < starAmount; i++) {
-  const star = {
-    sprite: new PIXI.Sprite(starTexture),
-    z: 0,
-    x: 0,
-    y: 0
-  };
-  star.sprite.anchor.x = 0.5;
-  star.sprite.anchor.y = 0.7;
-  randomizeStar(star, true);
-  app.stage.addChild(star.sprite);
-  stars.push(star);
-}
+    this.points = points;
 
-function randomizeStar(star, initial) {
-  star.z = initial
-    ? Math.random() * 2000
-    : cameraZ + Math.random() * 1000 + 2000;
+    this.lineStyle(s, c);
 
-  // Calculate star positions with radial random coordinate so no star hits the camera.
-  const deg = Math.random() * Math.PI * 2;
-  const distance = Math.random() * 50 + 1;
-  star.x = Math.cos(deg) * distance;
-  star.y = Math.sin(deg) * distance;
-}
-
-// Change flight speed every 5 seconds
-setInterval(() => {
-  warpSpeed = warpSpeed > 0 ? 0 : 1;
-}, 5000);
-
-// Listen for animate update
-app.ticker.add(delta => {
-  // Simple easing. This should be changed to proper easing function when used for real.
-  speed += (warpSpeed - speed) / 20;
-  cameraZ += delta * 10 * (speed + baseSpeed);
-  for (let i = 0; i < starAmount; i++) {
-    const star = stars[i];
-    if (star.z < cameraZ) randomizeStar(star);
-
-    // Map star 3d position to 2d with really simple projection
-    const z = star.z - cameraZ;
-    star.sprite.x =
-      star.x * (fov / z) * app.renderer.screen.width +
-      app.renderer.screen.width / 2;
-    star.sprite.y =
-      star.y * (fov / z) * app.renderer.screen.width +
-      app.renderer.screen.height / 2;
-
-    // Calculate star scale & rotation.
-    const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
-    const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
-    const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
-    const distanceScale = Math.max(0, (2000 - z) / 2000);
-    star.sprite.scale.x = distanceScale * starBaseSize;
-    // Star is looking towards center so that y axis is towards center.
-    // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
-    star.sprite.scale.y =
-      distanceScale * starBaseSize +
-      (distanceScale * speed * starStretch * distanceCenter) /
-        app.renderer.screen.width;
-    star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+    this.moveTo(points[0], points[1]);
+    this.lineTo(points[2], points[3]);
   }
-});
+
+  updatePoints(p) {
+    var points = (this.points = p.map(
+      (val, index) => val || this.points[index]
+    ));
+
+    var s = this.lineWidth,
+      c = this.lineColor;
+
+    this.clear();
+    this.lineStyle(s, c);
+    this.moveTo(points[0], points[1]);
+    this.lineTo(points[2], points[3]);
+  }
+}
+
+const firstPoint = [300, 300];
+const secondPoint = [280, 310];
+const thirdPoint = [150, 230];
+const fourthPoint = [150, 100];
+const fifthPoint = [280, 20];
+const sixthPoint = [410, 100];
+const seventhPoint = [410, 200];
+
+const lines = [
+  new Line(firstPoint.concat(secondPoint).flat()),
+  new Line(secondPoint.concat(thirdPoint).flat()),
+  new Line(thirdPoint.concat(fourthPoint).flat()),
+  new Line(fourthPoint.concat(fifthPoint).flat()),
+  new Line(fifthPoint.concat(sixthPoint).flat()),
+  new Line(sixthPoint.concat(seventhPoint).flat())
+];
+
+setup();
+
+function setup() {
+  const JS = new PIXI.Text("JS", {
+    font: "Montserrat",
+    align: "center",
+    fontSize: 130,
+    fontWeight: "bold"
+  });
+
+  JS.position.set(200, 80);
+
+  app.stage.addChild(JS);
+
+  setupDrawing();
+}
+
+function setupDrawing() {
+  const interval = setInterval(() => {
+    if (lines.length > 0) {
+      app.stage.addChild(lines.shift());
+    } else {
+      clearInterval(interval);
+      setupRepublic();
+    }
+  }, 200);
+}
+
+let textLength, textString, style, displayText, count;
+
+function setupRepublic() {
+  //Create a javascript string object with input text
+  textString = String("REPUBLIC");
+
+  //Get the string length
+  textLength = textString.length;
+
+  style = {
+    font: "Montserrat",
+    align: "center",
+    fontWeight: "bold"
+  };
+
+  displayText = new PIXI.Text("", style);
+  displayText.anchor.set(1, 0);
+  displayText.position.set(420, 230);
+
+  count = 0;
+  animateRepublic();
+}
+
+function animateRepublic() {
+  //Update display text length
+  let length = (textLength * count) / 60;
+
+  //Grab a a substring of the input text beginning with the first character
+  let string = textString.substr(0, length);
+
+  //Set the text of displayText object equal to the substring above
+  displayText.text = string;
+
+  //Add the displayText to the stage and render
+  app.stage.addChild(displayText);
+  app.renderer.render(app.stage);
+
+  requestAnimationFrame(animateRepublic);
+
+  count++;
+}
